6b8b7b457a40b12ebdb8914a4923cc07071bfa32
cladiuzzz <claudio.snaida18@gmail.com>
2026-04-25 00:42:56 -0500

    optimización de imagenes y ruta de placeholders

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true, // ESTO SOLUCIONA EL ERROR 400
+  },
+  // Si tienes la línea "output: 'export'", asegúrate de que esté comentada para Vercel
+  // output: 'export', 
 };
+
+export default nextConfig;
